6abde5b47f0bb312d4d2a9d40c0032e41d44a03a
Vlad Lavrishko <lavrishkovlad@gmail.com>
2023-03-09 13:11:15 +0200

    rework algoritm

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -7,11 +7,17 @@ using HtmlAgilityPack;
 using OfficeOpenXml;
 using System.IO;
 using Microsoft.Win32;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Reflection;
+using VisioForge.Core.VideoEdit.Timeline.Timeline;
 
 namespace TIkTokStats
 {
     public partial class MainWindow : Window
     {
+        private int index = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -21,89 +27,117 @@ namespace TIkTokStats
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            ViewInfoOfUser();
+            ViewInfoOfUser(true, null);
         }
 
-        private void ViewInfoOfUser()
+        private async void ViewInfoOfUser(bool isNew, OpenFileDialog openFileDialog)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            bool? result = openFileDialog.ShowDialog();
-            if (result == false || result == null) return;
-            List<string> userUrl = ParseUrlUserFromExcel(openFileDialog);
-            List<string>[] userInfo = new List<string>[userUrl.Count];
-            for (int i = 0; i < userUrl.Count; i++)
+            try
             {
-                var web = new HtmlWeb();
-                var doc = web.Load(userUrl[i]);
-                userInfo[i] = new List<string>
+                if (isNew)
                 {
-                    doc.DocumentNode.SelectSingleNode("//strong[@title='Likes']").InnerHtml,
-                    doc.DocumentNode.SelectSingleNode("//strong[@title='Followers']").InnerHtml
-                };
-            }
-            SaveInExcel(openFileDialog, userInfo);
-        }
-
-        private void SaveInExcel(OpenFileDialog openFileDialog, List<string>[] userInfo)
-        {
-            Stream stream;
-            if ((stream = openFileDialog.OpenFile()) != null)
-            {
-                var newFile = new FileInfo(openFileDialog.FileName);
-                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-                using (var package = new ExcelPackage(newFile))
+                    index = 0;
+                    OpenFileDialog openFileDialogTemp = new OpenFileDialog();
+                    bool? result = openFileDialogTemp.ShowDialog();
+                    if (result == false || result == null) return;
+                    openFileDialog = openFileDialogTemp;
+                }
+                if (openFileDialog != null)
                 {
-                    stream.Close();
-                    ExcelWorksheet worksheet = CheckExcelPage(package);
-                    worksheet.Cells[1, 1].Value = "User Url";
-                    worksheet.Cells[1, 2].Value = "User Likes";
-                    worksheet.Cells[1, 3].Value = "User Followers";
-                    for (int i = 0; i < userInfo.Length; i++)
+                    Stream stream = stream = openFileDialog.OpenFile();
+                    List<string> userUrl = await ParseUrlUserFromExcelAsync(openFileDialog);
+                    List<string>[] userInfo = new List<string>[userUrl.Count];
+                    for (int i = index; i < userUrl.Count; i++)
                     {
+                        var web = new HtmlWeb();
+                        var doc = await web.LoadFromWebAsync(userUrl[i]);
+                        userInfo[i] = new List<string>
+                        {
+                            doc.DocumentNode.SelectSingleNode("//strong[@title='Likes']").InnerHtml,
+                            doc.DocumentNode.SelectSingleNode("//strong[@title='Followers']").InnerHtml
+                        };
                         for (int j = 0; j < userInfo[i].Count; j++)
                         {
-                            worksheet.Cells[i + 2, j + 2].Value = userInfo[i][j];
+                            await SaveInExcelAsync(stream, openFileDialog.FileName, i, j, userInfo[i][j]);
                         }
+                        Progress.Text = ((float)(i + 1) / (float)userUrl.Count * 100).ToString("0") + "%";
+                        index = i;
                     }
-                    package.Save();
+                    stream.Close();
                     ShowMessageBox();
                 }
             }
+            catch (System.NullReferenceException)
+            {
+                ViewInfoOfUser(false, openFileDialog);
+                MessageBox.Show("Помилка в запусу одного файлу на " + (index + 2) + " рядку", 
+                    "Помилка", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
         }
-        private List<string> ParseUrlUserFromExcel(OpenFileDialog openFileDialog)
+
+        private async Task<List<string>> ParseUrlUserFromExcelAsync(OpenFileDialog openFileDialog)
         {
             Stream stream;
-            if ((stream = openFileDialog.OpenFile()) != null)
+            try
             {
-                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-                var newFile = new FileInfo(openFileDialog.FileName);
-                using (var package = new ExcelPackage(newFile))
+                if ((stream = openFileDialog.OpenFile()) != null)
                 {
-                    stream.Close();
-                    ExcelWorksheet worksheet = CheckExcelPage(package);
-                    string userUrl = String.Empty;
-                    List<string> usersUrl = new List<string>();
-                    int index = 2;
-                    do
+                    ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                    var newFile = new FileInfo(openFileDialog.FileName);
+                    using (var package = new ExcelPackage(newFile))
                     {
-                        var cell = worksheet.Cells[index, 1];
-                        userUrl = cell.Value?.ToString() ?? string.Empty;
-                        if (string.IsNullOrEmpty(userUrl)) break;
-                        usersUrl.Add(userUrl);
-                        index++;
-                    } while (!string.IsNullOrEmpty(userUrl));
-                    return usersUrl;
+                        await Task.Run(() => stream.Close());
+                        ExcelWorksheet worksheet = await CheckExcelPageAsync(package); 
+                        string userUrl = String.Empty;
+                        List<string> usersUrl = new List<string>();
+                        int index = 2;
+                        do
+                        {
+                            var cell = worksheet.Cells[index, 1];
+                            userUrl = cell.Value?.ToString() ?? string.Empty;
+                            if (string.IsNullOrEmpty(userUrl)) break;
+                            usersUrl.Add(userUrl);
+                            index++;
+                        } while (!string.IsNullOrEmpty(userUrl));
+                        return usersUrl;
+                    }
                 }
+            }
+            catch (System.NullReferenceException)
+            {
+                MessageBox.Show("Помилка в парсенгу url", "Помилка", MessageBoxButton.OK, MessageBoxImage.Information);
             }
             return null;
         }
-        private static ExcelWorksheet CheckExcelPage(ExcelPackage package)
+
+        private async Task SaveInExcelAsync(Stream stream, string path, int indexCellsX, int indexCellsY, string value)
+        {
+            var newFile = new FileInfo(path);
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (var package = new ExcelPackage(newFile))
+            {
+                await Task.Run(() => stream.Close());
+                ExcelWorksheet worksheet = await CheckExcelPageAsync(package);
+                SetBaseInfo(worksheet);
+                worksheet.Cells[indexCellsX + 2, indexCellsY + 2].Value = value;
+                await Task.Run(() => package.Save());
+            }
+        }
+        private static async Task<ExcelWorksheet> CheckExcelPageAsync(ExcelPackage package)
         {
             ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
             if (worksheet == null)
-                package.Workbook.Worksheets.Add("WorkSheet1");
-            worksheet = package.Workbook.Worksheets[0];
+            {
+                await Task.Run(() => package.Workbook.Worksheets.Add("WorkSheet1"));
+                worksheet = package.Workbook.Worksheets[0];
+            }
             return worksheet;
+        }
+        private static void SetBaseInfo(ExcelWorksheet worksheet)
+        {
+            worksheet.Cells[1, 1].Value = "User Url";
+            worksheet.Cells[1, 2].Value = "User Likes";
+            worksheet.Cells[1, 3].Value = "User Followers";
         }
         private static void ShowMessageBox()
         {
